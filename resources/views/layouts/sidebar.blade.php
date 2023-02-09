7d8b54c1f5557bc645b191a9618a3d2b8074f26d
djgraphics28 <darwin.ibay@30@gmail.com>
2023-02-09 22:28:45 +0800

    darz

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -85,6 +85,15 @@
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="/logout" class="nav-link">
+              <i class="nav-icon fas fa-building"></i>
+              <p>
+                Logout
+                {{-- <span class="right badge badge-danger">New</span> --}}
+              </p>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
